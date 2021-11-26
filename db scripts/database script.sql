774f952654634f1b442fa2af4e68ac153cc059bd
anilm <anilmerugu17@gmail.com>
2021-11-26 12:19:31 -0600

    Database desig updated

--- db scripts/database script.sql
+++ db scripts/database script.sql
@@ -7,20 +7,14 @@ CREATE TABLE STUTOR_TABLE(email varchar(30) primary key,
                         password varchar(20) NOT NULL,
                         user_type varchar(10) NOT NULL);
 
-CREATE TABLE STUDENT_PROFILE (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                            subject_name varchar(30) NOT NULL,
+CREATE TABLE STUDENT_PROFILE (subject_name varchar(30) NOT NULL,
                             edu_level varchar(30) DEFAULT NULL,
                             pay_per_hour INT,
-                            email varchar(45) NOT NULL,
-                            KEY `for1_idx` (email),
-                            CONSTRAINT `for1` FOREIGN KEY (email) REFERENCES
-                            STUTOR_TABLE(email) ON DELETE NO ACTION ON UPDATE NO ACTION);
+                            email varchar(45) PRIMARY KEY
+                            );
 
-CREATE TABLE TUTOR_PROFILE(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                            subject_name varchar(30)  NOT NULL,
+CREATE TABLE TUTOR_PROFILE(subject_name varchar(30)  NOT NULL,
                             edu_level varchar(30) DEFAULT NULL,
                             pay_per_hour INT,
-                            email varchar(45)  NOT NULL,
-                            KEY `for2_idx` (email),
-                            CONSTRAINT `for2` FOREIGN KEY (email) REFERENCES
-                            STUTOR_TABLE(email) ON DELETE NO ACTION ON UPDATE NO ACTION);
+                            email varchar(45) PRIMARY KEY
+                            );
